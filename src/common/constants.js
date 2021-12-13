a3f55e1e9517f96ff8433fb0d46741664f1247f2
Artyom <jyjyjyj3@gmail.com>
2021-12-13 21:07:45 +0300

    feature: add constants

---
+++ src/common/constants.js
@@ -0,0 +1,3 @@
+export const SMALL_DEVICES_BREAKPOINT = '(max-width: 767.98px)'
+export const MEDIUM_DEVICES_BREAKPOINT = '(max-width: 991.98px)'
+export const LARGE_DEVICES_BREAKPOINT = '(min-width: 1199.98px)'
